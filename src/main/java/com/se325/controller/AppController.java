838dd91f89a9ff028940adfde22a33d678a6c9e0
janodyamoonamale <jano.m92@gmail.com>
2013-09-09 16:03:47 +1200

    logging in now displays actual username not just 64bit number

--- src/main/java/com/se325/controller/AppController.java
+++ src/main/java/com/se325/controller/AppController.java
@@ -1,17 +1,38 @@
 package com.se325.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 @Controller
 //@RequestMapping("/main")
 public class AppController{
 	
+	private static String username64Bit;
 	private static String username;
 
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
@@ -29,9 +50,10 @@ public class AppController{
 	}
 	
 	@RequestMapping(value = "/common", method = RequestMethod.GET)
-	public String printLoggedIn(ModelMap model) {
+	public String printLoggedIn(ModelMap model) throws ParserConfigurationException, MalformedURLException, SAXException, IOException{
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		username = this.get64BitId(auth.getName());
+		username64Bit = this.get64BitId(auth.getName());//gets the 64 bit user name
+		username = this.getSteamUsername(username64Bit);//gets your display name for steam
 		model.addAttribute("message", "Hello! "+username+" You Have Logged In choose file to upload");
 		return "loggedIn_home";
 	}
@@ -51,6 +73,20 @@ public class AppController{
 	
 	private String get64BitId(String claimedId){		
 		return claimedId.substring(claimedId.indexOf("/id/")+4);
+	}
+	
+	private String getSteamUsername(String username) throws ParserConfigurationException, MalformedURLException, SAXException, IOException{
+		
+		//getting xml of your steam profile which contains display name
+		DocumentBuilderFactory fac = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = fac.newDocumentBuilder();
+		Document document = builder.parse(new URL("http://steamcommunity.com/profiles/"+username+"/?xml=1").openStream());
+		
+		//extracting the "<steamID>" tag which contains display name
+		NodeList rootElement = document.getElementsByTagName("steamID");
+		
+		return rootElement.item(0).getTextContent();
+		
 	}
 
 }
