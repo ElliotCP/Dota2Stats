1097077d98992ad821318ed54cf00955993c66c9
Elliot <elliot.colquhoun@gmail.com>
2013-09-10 14:28:00 +1200

    Added multiprocessing, and more data scraping. Not sure if multiprocessing is done properly..

--- testReplaysAndData/getPlayerStats.py
+++ testReplaysAndData/getPlayerStats.py
@@ -1,6 +1,9 @@
 import sys
 import os
-import json
+import json 
+import Image
+import ImageDraw
+from multiprocessing import Process
 
 global json_data
 # to be set
@@ -13,20 +16,24 @@ chosenPlayer = "dd Funzii"
 replayNumber = "194861937"
 #---------------------------
 
+# Initialise globals
 json_data=open("json/" + replayNumber + "/" + "players" + ".json")
 
-def loadJSON(jsonName) :
-	json_data=open("json/" + replayNumber + "/" + jsonName + ".json")
-	return json.load(json_data)
-
-def closeJSON() :
-	json_data.close()
-
-
+playerList = []
 playerGoldOverTime = []
+playerTotalGoldOverTime = []
+playerGPMOverTime = []
 playerXPOverTime = []
 playerLevelOverTime = []
 playerCSOverTime = []
+playerItemProgressionOverTime = []
+playerKillsOverTime = []
+playerAssistsOverTime = []
+playerDeathsOverTime = []
+playerBuildingKillsOverTime = []
+playerDamageDealtOverTime = []
+playerDamageTakenOverTime = []
+playerBuybackOverTime = []
 
 playerTotalCS = 0
 playerTotalDenies = 0
@@ -39,105 +46,367 @@ playerTotalDenyNeuts = 0
 playerTotalGold = 0
 playerTotalXP = 0
 playerMaxLevel = 0
-#-----------------------------------------------------------
-# LOAD PLAYER DATA
+playerTotalDamageDealt = 0
+playerTotalDamageTaken = 0
+playerTotalDivines = 0
 
-players = loadJSON("players")
+def loadJSON(jsonName) :
+	json_data=open("json/" + replayNumber + "/" + jsonName + ".json")
+	return json.load(json_data)
 
-for field in players["players"] :
-	playerName = field["player"]
-	hero = field["hero"]
-	players[playerName] = hero
+def closeJSON() :
+	json_data.close()
 
-playerObserved = {"name" : chosenPlayer, "hero" : players[chosenPlayer]}
+def generatePlayerLevelGraph(playerLevelOverTime) :
+	imageWidth = 800.0
+	imageHeight = 500.0
+	im = Image.new("RGB", (800, 500), "white")
+	draw = ImageDraw.Draw(im)
+	maxTime = float(playerLevelOverTime[-1][0])
+	maxLevel = float(playerLevelOverTime[-1][1])
+	draw.line((0, imageHeight, float(playerLevelOverTime[0][0])/maxTime*imageWidth, imageHeight-float(playerLevelOverTime[0][1])/maxLevel*imageHeight), fill=(0,0,0))
+	for i in range(len(playerLevelOverTime)-1) :
+		draw.line((float(playerLevelOverTime[i][0])/maxTime*imageWidth, imageHeight-float(playerLevelOverTime[i][1])/maxLevel*imageHeight, float(playerLevelOverTime[i+1][0])/maxTime*imageWidth, imageHeight-float(playerLevelOverTime[i+1][1])/maxLevel*imageHeight), fill=(0,0,0))
+		draw.text((float(playerLevelOverTime[i+1][0])/maxTime*imageWidth, imageHeight-30.0), str(playerLevelOverTime[i+1][1]).title(), fill=(255,0,0))
 
-closeJSON()
+	del draw
 
-#-----------------------------------------------------------
+	im.save("playerLevelGraph.png")
 
-#-----------------------------------------------------------
-# GET GOLD INFO
+def generatePlayerGoldGraph(playerTotalGoldOverTime) :
+	imageWidth = 800.0
+	imageHeight = 500.0
+	im = Image.new("RGB", (800, 500), "white")
+	draw = ImageDraw.Draw(im)
+	maxTime =  float(playerTotalGoldOverTime[-1][0])
+	maxGold = float(playerTotalGoldOverTime[-1][1])
+	draw.line((0, imageHeight, float(playerTotalGoldOverTime[0][0])/maxTime*imageWidth, imageHeight-float(playerTotalGoldOverTime[0][1])/maxGold*imageHeight), fill=(0,0,0))
+	for i in range(len(playerTotalGoldOverTime)-1) :
+		draw.line((float(playerTotalGoldOverTime[i][0])/maxTime*imageWidth, imageHeight-float(playerTotalGoldOverTime[i][1])/maxGold*imageHeight, float(playerTotalGoldOverTime[i+1][0])/maxTime*imageWidth, imageHeight-float(playerTotalGoldOverTime[i+1][1])/maxGold*imageHeight), fill=(0,0,0))
+		#draw.text((float(playerTotalGoldOverTime[i+1][0])/float(maxTime)*imageWidth, float(playerTotalGoldOverTime[i+1][1])/float(maxGold)*imageHeight), str(playerTotalGoldOverTime[i+1][1]).title(), fill=(255,0,0))
 
-gold = loadJSON("gold")
+	del draw
 
-for field in gold["gold"] :
-	if field["hero"] ==  playerObserved["hero"] :
+	im.save("playerGoldGraph.png")
+
+def generatePlayerGPMGraph(playerGPMOverTime) :
+	imageWidth = 800.0
+	imageHeight = 500.0
+	im = Image.new("RGB", (800, 500), "white")
+	draw = ImageDraw.Draw(im)
+	maxTime = float(playerGPMOverTime[-1][0])
+	maxGPM = 0
+	for i in range(len(playerGPMOverTime)) :
+		if playerGPMOverTime[i][1] > maxGPM :
+			maxGPM = float(playerGPMOverTime[i][1])
+
+	draw.line((0, imageHeight, float(playerGPMOverTime[0][0])/maxTime*imageWidth, imageHeight-float(playerGPMOverTime[0][1])/maxGPM*imageHeight), fill=(0,0,0))
+	for i in range(len(playerGPMOverTime)-1) :
+		draw.line((float(playerGPMOverTime[i][0])/maxTime*imageWidth, imageHeight-float(playerGPMOverTime[i][1])/maxGPM*imageHeight, float(playerGPMOverTime[i+1][0])/maxTime*imageWidth, imageHeight-float(playerGPMOverTime[i+1][1])/maxGPM*imageHeight), fill=(0,0,0))
+		#draw.text((playerGPMOverTime[i+1][0]/10, playerGPMOverTime[i+1][1]*10), str(playerGPMOverTime[i+1][1]).title(), fill=(255,0,0))
+
+	del draw
+
+	im.save("playerGPMGraph.png")
+
+def generatePlayerDamageDealtGraph(playerDamageDealtOverTime) :
+	imageWidth = 800.0
+	imageHeight = 500.0
+	im = Image.new("RGB", (800, 500), "white")
+	draw = ImageDraw.Draw(im)
+	maxTime = float(playerDamageDealtOverTime[-1][0])
+	maxDamage = 0
+	for i in range(len(playerDamageDealtOverTime)) :
+		if playerDamageDealtOverTime[i][2] > maxDamage :
+			maxDamage = float(playerDamageDealtOverTime[i][2])
+	if maxDamage == 0 :
+		pass
+	draw.line((0, imageHeight, float(playerDamageDealtOverTime[0][0])/maxTime*imageWidth, imageHeight-float(playerDamageDealtOverTime[0][2])/maxDamage*imageHeight), fill=(0,0,0))
+	for i in range(len(playerDamageDealtOverTime)-1) :
+		draw.line((float(playerDamageDealtOverTime[i][0])/maxTime*imageWidth, imageHeight-float(playerDamageDealtOverTime[i][2])/maxDamage*imageHeight, float(playerDamageDealtOverTime[i+1][0])/maxTime*imageWidth, imageHeight-float(playerDamageDealtOverTime[i+1][2])/maxDamage*imageHeight), fill=(0,0,0))
+		#draw.text((playerDamageDealtOverTime[i+1][0]/10, playerDamageDealtOverTime[i+1][1]*10), str(playerDamageDealtOverTime[i+1][1]).title(), fill=(255,0,0))
+
+	del draw
+
+	im.save("playerDamageDealtGraph.png")
+
+def generatePlayerDamageDealtSpecificGraph(playerDamageDealtOverTime, dealtTo) :
+	imageWidth = 800.0
+	imageHeight = 500.0
+	im = Image.new("RGB", (800, 500), "white")
+	draw = ImageDraw.Draw(im)
+	maxTime = float(playerDamageDealtOverTime[-1][0])
+	maxDamage = 0
+	for i in range(len(playerDamageDealtOverTime)) :
+		if playerDamageDealtOverTime[i][2] > maxDamage and dealtTo in playerDamageDealtOverTime[i][1] :
+			maxDamage = float(playerDamageDealtOverTime[i][2])
+
+	draw.line((0, imageHeight, float(playerDamageDealtOverTime[0][0])/maxTime*imageWidth, imageHeight-float(playerDamageDealtOverTime[0][2])/maxDamage*imageHeight), fill=(0,0,0))
+	for i in range(len(playerDamageDealtOverTime)-1) :
+		if dealtTo in playerDamageDealtOverTime[i][1] :
+			draw.line((float(playerDamageDealtOverTime[i][0])/maxTime*imageWidth, imageHeight-float(playerDamageDealtOverTime[i][2])/maxDamage*imageHeight, float(playerDamageDealtOverTime[i+1][0])/maxTime*imageWidth, imageHeight-float(playerDamageDealtOverTime[i+1][2])/maxDamage*imageHeight), fill=(0,0,0))
+		#draw.text((playerDamageDealtOverTime[i+1][0]/10, playerDamageDealtOverTime[i+1][1]*10), str(playerDamageDealtOverTime[i+1][1]).title(), fill=(255,0,0))
+
+	del draw
+
+	im.save("playerDamageDealtTo" + dealtTo.replace("npc_dota_hero_","").replace("_", " ").title().replace(" ", "") + "Graph.png")
+
+
+
+#def loadData() :
+
+	
+if __name__ == '__main__' :
+	players = loadJSON("players")
+
+
+	#-----------------------------------------------------------
+	# LOAD PLAYER DATA
+	for field in players["players"] :
+		playerName = field["player"]
+		hero = field["hero"]
+		if playerName == chosenPlayer :
+			chosenHero = hero
+		playerList.append((playerName, hero))
+
+	playerObserved = {"name" : chosenPlayer, "hero" : hero}
+
+	closeJSON()
+
+	#-----------------------------------------------------------
+
+	#-----------------------------------------------------------
+	# GET GOLD INFO
+
+	gold = loadJSON("gold")
+
+	for field in gold["gold"] :
+		if field["hero"] ==  playerObserved["hero"] :
+			time = field["time"]
+			gold = field["gold"]
+			playerGoldOverTime.append((time, gold))
+			playerTotalGold += field["gold"]
+			playerTotalGoldOverTime.append((time, playerTotalGold))
+			playerGPMOverTime.append((time, playerTotalGold/(time/60)))
+
+	closeJSON()
+	#-----------------------------------------------------------
+
+	#-----------------------------------------------------------
+	# GET LEVELUP INFORMATION
+
+	levelup = loadJSON("levelups")
+
+	for field in levelup["leveluptimes"] :
+		if field["hero"] == playerObserved["hero"] :
+			time = field["time"]
+			level = field["level"]
+			playerLevelOverTime.append((time, level))
+
+	closeJSON()
+
+	#-----------------------------------------------------------
+
+	#-----------------------------------------------------------
+	# GET CS INFORMATION
+
+	cs = loadJSON("cs")
+
+	for field in cs["cs"] :
+		if field["hero"] == playerObserved["hero"] :
+			time = field["time"]
+			creepKilled = field["kill"]
+			if creepKilled.startswith("npc_dota_creep_badguys_melee") :
+				playerTotalEnemyMeleeCreep += 1
+			elif creepKilled.startswith("npc_dota_creep_badguys_range") :
+				playerTotalEnemyRangeCreep += 1
+			elif creepKilled.startswith("npc_dota_neutral") :
+				playerTotalNeuts += 1
+
+			playerTotalCS += 1
+			playerCSOverTime.append((time, creepKilled, creepKilled))
+
+	closeJSON()
+
+	#-----------------------------------------------------------
+
+	#-----------------------------------------------------------
+	# GET DENY INFORMATION
+	 
+	denies = loadJSON("denies")
+
+	for field in denies["denies"] :
+		if field["hero"] == playerObserved["hero"] :
+			time = field["time"]
+			creepKilled = field["kill"]
+			if creepKilled.startswith("npc_dota_creep_badguys_melee") :
+				playerTotalDenyMeleeCreep += 1
+			elif creepKilled.startswith("npc_dota_creep_badguys_range") :
+				playerTotalDenyRangeCreep += 1
+			elif creepKilled.startswith("npc_dota_neutral") :
+				playerTotalDenyNeuts += 1
+
+			playerTotalDenies += 1
+			playerCSOverTime.append((time, creepKilled, creepKilled))
+
+	closeJSON()
+
+	#-----------------------------------------------------------
+
+	#-----------------------------------------------------------
+	# GET ITEM PROGRESSION
+
+	itemtimes = loadJSON("itemtimes")
+
+	for field in itemtimes["itemtimes"] :
+		if field["hero"] == playerObserved["hero"] :
+			time = field["time"]
+			item = field["item"]
+			if "divine" in item :
+				playerTotalDivines += 1
+			playerItemProgressionOverTime.append((time, item))
+
+	closeJSON()
+
+	#-----------------------------------------------------------
+
+	#-----------------------------------------------------------
+	# GET KILLS INFO
+
+	herokills = loadJSON("herokills")
+
+	for field in herokills["herokills"] :
+		if field["killer"] == playerObserved["hero"] :
+			time = field["time"]
+			dead = field["dead"]
+			playerKillsOverTime.append((time, dead))
+		if playerObserved["hero"] in field["assists"] :
+			time = field["time"]
+			dead = field["dead"]
+			playerAssistsOverTime.append((time, dead))
+		if field["dead"] == playerObserved["hero"] :
+			time = field["time"]
+			killedBy = field["killer"]
+			playerDeathsOverTime.append((time, killedBy))		
+
+	closeJSON()
+
+	#-----------------------------------------------------------
+
+	#-----------------------------------------------------------
+	# GET TOWER INFO
+
+	buildings = loadJSON("buildings")
+
+	for field in buildings["buildingtimers"] :
+		if field["killer"] == playerObserved["hero"] :
+			time = field["time"]
+			building = field["building"]
+			playerBuildingKillsOverTime.append((time, building))
+
+	closeJSON()
+
+	#-----------------------------------------------------------
+
+	#-----------------------------------------------------------
+	# GET RUNE INFO
+
+	runes = loadJSON("runes")
+
+	for field in runes["runes"] :
+		if field["hero"] == playerObserved["hero"] :
+			time = field["time"]
+
+			if field["rune"] == 0 :
+				rune = "double_damage"
+			elif field["rune"] == 1 :
+				rune = "haste"
+			elif field["rune"] == 2 :
+				rune = "illusion"
+			elif field["rune"] == 3 :
+				rune = "regen"
+			elif field["rune"] == 4 :
+				rune = "invisibility"
+
+			bottle = field["bottle"]
+			playerBuildingKillsOverTime.append((time, rune, bottle))
+
+	closeJSON()
+
+	#-----------------------------------------------------------
+
+	#-----------------------------------------------------------
+	# GET COMBAT LOG INFO
+
+	combatlog = loadJSON("combatlog")
+
+	for field in combatlog["combatlog"] :
 		time = field["time"]
-		gold = field["gold"]
-		playerGoldOverTime.append((time, gold))
-		playerTotalGold += int(field["gold"])
+		damage = field["value"]
+		if field["source"] == playerObserved["hero"] :
+			target = field["target"]
+			playerTotalDamageDealt += damage
+			playerDamageDealtOverTime.append((time, target, damage))
+		if field["target"] == playerObserved["hero"] :
+			damageFrom = field["source"]
+			playerTotalDamageTaken += damage
+			playerDamageTakenOverTime.append((time, damageFrom, damage))
 
-closeJSON()
-#-----------------------------------------------------------
+	closeJSON()
 
-#-----------------------------------------------------------
-# GET LEVELUP INFORMATION
+	#-----------------------------------------------------------
 
-levelup = loadJSON("levelups")
+	#-----------------------------------------------------------
+	# GET BUYBACK INFO
 
-for field in levelup["leveluptimes"] :
-	if field["hero"] == playerObserved["hero"] :
-		time = field["time"]
-		level = field["level"]
-		playerLevelOverTime.append((time, level))
+	buybacks = loadJSON("buybacks")
 
-closeJSON()
+	for field in buybacks["buybacks"] :
+		if field["player"] == playerObserved["hero"] :
+			time = field["time"]
+			for i in range(len(playerLevelOverTime)) :
+				if time >= playerLevelOverTime[i][0] :
+					buybackLevel = playerLevelOverTime[i][1]
+			level = buybackLevel
+			playerBuybackOverTime.append((time, level))
 
-#-----------------------------------------------------------
+	closeJSON()
 
-#-----------------------------------------------------------
-# GET CS INFORMATION
+	#-----------------------------------------------------------
 
-cs = loadJSON("cs")
+	# Multiprocessing (is this done properly?)
+	playerLevelProcess = Process(target=generatePlayerLevelGraph, args=(playerLevelOverTime,))
+	playerLevelProcess.start()
+	playerLevelProcess.join()
+	playerGoldProcess = Process(target=generatePlayerGoldGraph, args=(playerTotalGoldOverTime,))
+	playerGoldProcess.start()
+	playerGoldProcess.join()
+	playerGPMProcess = Process(target=generatePlayerGPMGraph, args=(playerGPMOverTime,))
+	playerGPMProcess.start()
+	playerGPMProcess.join()
+	playerDamageDealtProcess = Process(target=generatePlayerDamageDealtGraph, args=(playerDamageDealtOverTime,))
+	playerDamageDealtProcess.start()
+	playerDamageDealtProcess.join()
 
-for field in cs["cs"] :
-	if field["hero"] == playerObserved["hero"] :
-		time = field["time"]
-		creepKilled = field["kill"]
-		if creepKilled.startswith("npc_dota_creep_badguys_melee") :
-			playerTotalEnemyMeleeCreep += 1
-		elif creepKilled.startswith("npc_dota_creep_badguys_range") :
-			playerTotalEnemyRangeCreep += 1
-		elif creepKilled.startswith("npc_dota_neutral") :
-			playerTotalNeuts += 1
+	for i in range(len(playerList)) :
+		playerDamageDealtSpecificProcess = Process(target=generatePlayerDamageDealtSpecificGraph, args=(playerDamageDealtOverTime, playerList[i][1]))
+		playerDamageDealtSpecificProcess.start()
+		playerDamageDealtSpecificProcess.join()
 
-		playerTotalCS += 1
-		playerCSOverTime.append((time, creepKilled, creepKilled))
-
-closeJSON()
-
-#-----------------------------------------------------------
-
-#-----------------------------------------------------------
-# GET DENY INFORMATION
- 
-denies = loadJSON("denies")
-
-for field in denies["denies"] :
-	if field["hero"] == playerObserved["hero"] :
-		time = field["time"]
-		creepKilled = field["kill"]
-		if creepKilled.startswith("npc_dota_creep_badguys_melee") :
-			playerTotalDenyMeleeCreep += 1
-		elif creepKilled.startswith("npc_dota_creep_badguys_range") :
-			playerTotalDenyRangeCreep += 1
-		elif creepKilled.startswith("npc_dota_neutral") :
-			playerTotalDenyNeuts += 1
-
-		playerTotalDenies += 1
-		playerCSOverTime.append((time, creepKilled, creepKilled))
-
-closeJSON()
-
-#-----------------------------------------------------------
+# Print statements for debugging
+# print "Player " + playerObserved["name"] + " earned " + str(playerTotalGold) + " gold during the match."
+# print "They also reached level " + str(playerLevelOverTime[-1][1])
+# print "CS: " + str(playerTotalCS) + ", Denies: " + str(playerTotalDenies)
+# print "This comprises of " + str(playerTotalEnemyMeleeCreep+playerTotalEnemyRangeCreep) + " enemy creeps and " + str(playerTotalNeuts) + " neutrals killed."
+# print "Also, " + str(playerTotalDenyMeleeCreep+playerTotalDenyRangeCreep) + " enemy creeps and " + str(playerTotalDenyNeuts) + " neutrals denied."
+# print "Last item bought was " + str(playerItemProgressionOverTime[-1][1].replace("modifier_item_", "").replace("_", " ").title()) + "."
+# print "This was at " + str(playerItemProgressionOverTime[-1][0]/60/60) + " mins."
+# print "Number of kills: " + str(len(playerKillsOverTime)) 
+# print "Number of assists: " + str(len(playerAssistsOverTime))
+# print "Number of deaths: " + str(len(playerDeathsOverTime))
+# print "Buildings killed: "  + str(len(playerBuildingKillsOverTime))
+# print "Damage dealt: " + str(playerTotalDamageDealt)
+# print "Damage taken: " + str(playerTotalDamageTaken)
 
 
 
-
-
-
-print "Player " + playerObserved["name"] + " earned " + str(playerTotalGold) + " gold during the match."
-print "They also reached level " + str(playerLevelOverTime[-1][1])
-print "CS: " + str(playerTotalCS) + ", Denies: " + str(playerTotalDenies)
-print "This comprises of " + str(playerTotalEnemyMeleeCreep+playerTotalEnemyRangeCreep) + " enemy creeps and " + str(playerTotalNeuts) + " neutrals killed."
-print "Also, " + str(playerTotalDenyMeleeCreep+playerTotalDenyRangeCreep) + " enemy creeps and " + str(playerTotalDenyNeuts) + " neutrals denied."
